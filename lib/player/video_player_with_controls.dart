a19abcc9be031f4681a43985db43bf8b4276f44d
moimoi.dev <toshiaki.satsuma@gmail.com>
2025-11-09 01:37:57 +0900

    fix: key

--- lib/player/video_player_with_controls.dart
+++ lib/player/video_player_with_controls.dart
@@ -135,7 +135,7 @@ class _PlayerState extends State<_Player> {
         builder: (context, value, child) {
           final androidViewType = "$viewType$_textureId";
           return PlatformViewLink(
-            key: ValueKey(value.playbackStateChangedTimestamp),
+            key: ValueKey('${_textureId}_${value.isFullscreen}_${value.isPip}'),
             viewType: androidViewType,
             surfaceFactory: (context, controller) {
               return AndroidViewSurface(
